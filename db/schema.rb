0f0dcacc4c8bc8f3f51972b8b2fb7bee7bbb16ae
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-03-08 15:05:34 +0700

    add book

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308072756) do
+ActiveRecord::Schema.define(version: 20160308080120) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "number_of_pages"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
